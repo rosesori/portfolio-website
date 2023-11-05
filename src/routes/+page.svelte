80efe7999c04394400f15cb929c5dc3c6ebfaf90
Rose Soriano <rosesoriano.dtx@gmail.com>
2023-11-05 00:38:05 -0700

    Setting up Tailwind CSS in a SvelteKit project.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+    <h1 class="text-3xl text-gray-700 font-bold mb-5">
+        Rose Soriano
+    </h1>
+    <p class="text-gray-500 text-lg">
+       SvelteKit and Tailwind CSS in action
+    </p>
+    <p>
+        <a href="https://github.com/rosesori">github.com/rosesori</a>
+    </p>
+</div>
